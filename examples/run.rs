0329a3968f9819a42170d179908c52eb321648a2
Marc Haubenstock <marc.haubenstock@outlook.com>
2020-12-31 18:38:33 +0100

    cleanup

--- examples/run.rs
+++ examples/run.rs
@@ -1,5 +1,15 @@
-extern crate rust_realsense_win;
+use rust_realsense_win;
+use std::ffi::CStr;
 
 fn main() {
-    rust_realsense_win::it_works();
+
+    assert_eq!(rust_realsense_win::RS2_PRODUCT_LINE_SR300, 4);
+    unsafe { 
+        let c_str = rust_realsense_win::rs2_notification_category_to_string(rust_realsense_win::rs2_notification_category_RS2_NOTIFICATION_CATEGORY_FRAMES_TIMEOUT);
+        let slice = CStr::from_ptr(c_str);
+        println!("string buffer size without nul terminator: {}", slice.to_bytes().len());
+        let str_slice: &str = slice.to_str().unwrap();
+        println!("string: {}", str_slice);
+    };
+
 }
--- src/lib.rs
+++ src/lib.rs
@@ -3,17 +3,3 @@
 #![allow(non_snake_case)]
 include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
 
-use std::ffi::CStr;
-
-
-pub fn it_works() {
-    assert_eq!(RS2_PRODUCT_LINE_SR300, 4);
-    unsafe { 
-        let c_str = rs2_notification_category_to_string(rs2_notification_category_RS2_NOTIFICATION_CATEGORY_FRAMES_TIMEOUT);
-        let slice = CStr::from_ptr(c_str);
-        println!("string buffer size without nul terminator: {}", slice.to_bytes().len());
-        let str_slice: &str = slice.to_str().unwrap();
-        println!("string: {}", str_slice);
-    };
-
-}
